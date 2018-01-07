3615f40bfe37de84ee75a854733e04c64cb90782
enrique <enrique.melero@gmail.com>
2018-01-07 16:18:09 +0100

    Renamed environment variables REACT_APP_ADDRESS to REACT_APP_MYADDRESS                               REACT_APP_PARENT to  REACT_APP_ISPARENT Added some comments in App.js to signal default ports for testrpc and ganache

--- ChildrenWallet/smart-contract/truffle.js
+++ ChildrenWallet/smart-contract/truffle.js
@@ -2,9 +2,9 @@ module.exports = {
     networks : {
         development : {
             gas : 2500000,
-            from : "0x1781eb4f98acebdf358ba38d8f975a08ae2efd36",
+            from : "0x627306090abaB3A6e1400e9345bC60c78a8BEf57",
             host : "localhost",
-            port : 8545,
+            port : 7545,
             network_id : "*" // Match any network id
         }
     }
